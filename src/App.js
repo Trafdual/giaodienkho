9a8bf01e0ac4a43a45dd043c69f9c078be23499b
trafdual <traz08102003@gmail.com>
2024-08-06 16:59:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useRef } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+import './App.scss'
+import images from './assets/images'
+import { Image as ImageLogin } from './components/ImageLogin'
+
+function App () {
+  const [showPassword, setShowPassword] = useState(false)
+  const [isIconVisible, setIsIconVisible] = useState(false)
+  const containerRef = useRef()
+ 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='dualscreen1'>
+        <ImageLogin />
+      </div>
+      <div className='dualscreen2'>
+        <div className='divlogin'>
+          <h1>Đăng Nhập</h1>
+          <div className='divemail'>
+            <input className='email' placeholder='' />
+            <label className='label'>Email</label>
+          </div>
+          <div className='divpassword'>
+            <div className='divippass'>
+              <input
+                className='password'
+                type={showPassword ? 'text' : 'password'}
+                placeholder=''
+                onFocus={() => setIsIconVisible(true)}
+                ref={containerRef}
+              />
+              <label className='label'>Password</label>
+              {isIconVisible && (
+                <button className='eye' onClick={togglePasswordVisibility}>
+                  <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+                </button>
+              )}
+            </div>
+          </div>
+          <button className='btnLogin'>Đăng Nhập</button>
+          <div className='divcachkhac'>
+            <button className='btnfacebook'>
+              <img src={images.facebook} alt='' />
+            </button>
+            <button className='btngoogle'>
+              <img src={images.google} alt='' />
+            </button>
+          </div>
+          <div className='divRegister'>
+            <h3>Bạn chưa có tài khoản?</h3>
+            <h3 className='register'>Đăng ký</h3>
+          </div>
+          <div className='chinhsach'>
+            <h4>
+              TRANG WEB NÀY ĐƯỢC BẢO MẬT BỞI HCAPTCHA VÀ TUÂN THỦ THEO{' '}
+              <a href='https://www.hcaptcha.com/privacy'>
+                CHÍNH SÁCH QUYỀN RIÊNG TƯ
+              </a>{' '}
+              VÀ{' '}
+              <a href='https://www.hcaptcha.com/terms'>
+                ĐIỀU KHOẢN DỊCH VỤ CỦA HCAPTCHA
+              </a>{' '}
+              .
+            </h4>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+const images={
+    logo:require('./logo.svg').default,
+    noImage:require('./no-image.png').default,
+    facebook:require('./facebook.svg').default,
+    google:require('./google.svg').default,
+    tn1:require('./tn1.jpg').default,
+    tn2:require('./tn2.jpg').default,
+    tn3:require('./tn3.jpg').default
+}
+export default images
