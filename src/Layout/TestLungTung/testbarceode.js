b795077ab69d04e76b1b2ed5853422d08dae9756
trafdual <traz08102003@gmail.com>
2024-12-06 12:15:42 +0700

    f

--- src/Layout/TestLungTung/testbarceode.js
+++ src/Layout/TestLungTung/testbarceode.js
@@ -1,80 +1,40 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useRef, useState } from 'react'
-import { db,ref,set } from '~/firebase/firebase' // Import Firebase cấu hình của bạn
-import Quagga from 'quagga' // Import QuaggaJS
+import React from 'react'
+import BarcodeScannerComponent from 'react-qr-barcode-scanner'
+import './test.scss'
 
-function Testbarceode () {
-  const videoRef = useRef(null)
-  const [scannedBarcode, setScannedBarcode] = useState('')
-
-  useEffect(() => {
-    const startScanning = () => {
-      // Khởi tạo QuaggaJS
-      Quagga.init(
-        {
-          inputStream: {
-            name: 'Live',
-            type: 'LiveStream',
-            target: videoRef.current, // Đặt phần tử video
-            constraints: {
-              facingMode: 'environment' // Sử dụng camera sau
-            }
-          },
-          decoder: {
-            readers: [
-              'code_128_reader',
-              'ean_reader',
-              'ean_8_reader',
-              'upc_reader',
-              'upc_e_reader',
-              'code_39_reader'
-            ]
-          }
-        },
-        err => {
-          if (err) {
-            console.error('Error initializing Quagga:', err)
-            return
-          }
-          Quagga.start() // Bắt đầu quét
-        }
-      )
-
-      // Lắng nghe sự kiện khi quét thành công
-      Quagga.onDetected(result => {
-        const barcode = result.codeResult.code
-        setScannedBarcode(barcode)
-        alert(`Scanned Barcode: ${barcode}`)
-
-        // Lưu kết quả quét vào Firebase Database
-        const barcodeRef = ref(db, 'scannedBarcodes/' + barcode)
-        set(barcodeRef, {
-          barcodeText: barcode,
-          timestamp: new Date().toISOString()
-        })
-
-        Quagga.stop() // Dừng quét sau khi có mã
-      })
-    }
-
-    startScanning()
-
-    return () => {
-      if (videoRef.current) {
-        Quagga.stop() // Dừng quét khi component bị unmount
-      }
-    }
-  }, [])
-
+function Testbarceode ({
+  setData,
+  handleAddImel,
+  index,
+  scanning,
+  setScanning
+}) {
   return (
-    <div>
-      <h2>Quét Mã Vạch</h2>
-      <div>
-        <video ref={videoRef} style={{ width: '100%', height: 'auto' }} />
-      </div>
-      <h3>
-        {scannedBarcode ? `Mã quét: ${scannedBarcode}` : 'Chưa có mã quét'}
-      </h3>
+    <div className='scanner-container'>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) {
+            setData(result.text)
+            handleAddImel(index, result.text)
+            setScanning(false)
+          }
+        }}
+        videoConstraints={{
+          facingMode: 'environment',
+          width: { ideal: 1920 }, // Full HD width
+          height: { ideal: 1080 }
+        }}
+        stopStream={!scanning}
+      />
+        <div class='scanner-overlay'>
+          <div class='overlay-top'></div>
+          <div class='overlay-bottom'></div>
+          <div class='overlay-left'></div>
+          <div class='overlay-right'></div>
+          <div class='scanner-box'></div>
+        </div>
     </div>
   )
 }
