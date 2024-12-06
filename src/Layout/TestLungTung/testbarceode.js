0e354ac225fa58e32536635068e03ca1e10c0a62
trafdual <traz08102003@gmail.com>
2024-12-06 12:54:11 +0700

    f

--- src/Layout/TestLungTung/testbarceode.js
+++ src/Layout/TestLungTung/testbarceode.js
@@ -1,55 +1,42 @@
-import React, { useState, useRef } from 'react'
-import vision from '@google-cloud/vision'
+import React from 'react'
+import BarcodeScannerComponent from 'react-qr-barcode-scanner'
+import './test.scss'
 
-const client = new vision.ImageAnnotatorClient()
-
-function BarcodeScanner () {
-  const [scannedBarcode, setScannedBarcode] = useState('')
-  const videoRef = useRef(null)
-
-  const startScanning = () => {
-    const video = videoRef.current
-    const canvas = document.createElement('canvas')
-    const context = canvas.getContext('2d')
-
-    const captureFrame = () => {
-      canvas.width = video.videoWidth
-      canvas.height = video.videoHeight
-      context.drawImage(video, 0, 0, canvas.width, canvas.height)
-      const base64Image = canvas.toDataURL('image/jpeg')
-
-      // Gửi ảnh đến Google Vision API
-      const request = {
-        image: { content: base64Image.split(',')[1] }
-      }
-
-      client
-        .textDetection(request)
-        .then(results => {
-          const detections = results[0].textAnnotations
-          if (detections.length > 0) {
-            setScannedBarcode(detections[0].description)
-            alert(`Scanned Barcode: ${detections[0].description}`)
-          }
-        })
-        .catch(err => {
-          console.error('ERROR:', err)
-        })
-    }
-
-    setInterval(captureFrame, 1000) // Quét mỗi giây
-  }
-
+function Testbarceode ({
+  setData,
+  handleAddImel,
+  index,
+  scanning,
+  setScanning
+}) {
   return (
-    <div>
-      <h2>Quét Mã Vạch</h2>
-      <video ref={videoRef} style={{ width: '100%' }} autoPlay />
-      <button onClick={startScanning}>Bắt đầu quét</button>
-      <h3>
-        {scannedBarcode ? `Mã quét: ${scannedBarcode}` : 'Chưa có mã quét'}
-      </h3>
+    <div className='scanner-container'>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) {
+            setData(result.text)
+            handleAddImel(index, result.text)
+            setScanning(false)
+          }
+        }}
+        videoConstraints={{
+          facingMode: 'environment',
+          width: { ideal: 1920 }, // Full HD width
+          height: { ideal: 1080 }
+        }}
+        stopStream={!scanning}
+      />
+        <div class='scanner-overlay'>
+          <div class='overlay-top'></div>
+          <div class='overlay-bottom'></div>
+          <div class='overlay-left'></div>
+          <div class='overlay-right'></div>
+          <div class='scanner-box'></div>
+        </div>
     </div>
   )
 }
 
-export default BarcodeScanner
+export default Testbarceode
