58aa684c643a141d27ea82cd207368ef4bcc1806
trafdual <traz08102003@gmail.com>
2024-12-16 19:17:37 +0700

    f

---
+++ src/Layout/BanHangLayout/ModalAddKhachHang/dist/index.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "ModalAddKhachHang", {
+  enumerable: true,
+  get: function get() {
+    return _ModalAddKhachHang["default"];
+  }
+});
+
+var _ModalAddKhachHang = _interopRequireDefault(require("./ModalAddKhachHang"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
